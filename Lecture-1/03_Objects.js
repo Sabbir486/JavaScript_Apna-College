91b352ab68e265d68810844868e04f22d4be38a3
Sabbir Ahmed <124485356+Sabbir486@users.noreply.github.com>
2025-05-01 23:28:23 +0600

    Add files via upload

---
+++ Lecture-1/03_Objects.js
@@ -0,0 +1,23 @@
+//Object --> Collection of different DataTypes
+
+const student = {
+    //key : value (pair)
+    fName : "Sabbir",
+    marks : 92,
+    cgpa : 3.81,
+    isPass : true
+};
+
+console.log(student);
+
+console.log(student["fName"]);
+console.log(student.marks);
+
+console.log(typeof(student));
+
+
+//Another Way
+student.marks = student.marks + 5;
+console.log(student.marks);
+
+
