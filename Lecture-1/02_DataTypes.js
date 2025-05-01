9f4e3313b1527f3c4be6f7333dc46fbc8fdfeef2
Sabbir Ahmed <124485356+Sabbir486@users.noreply.github.com>
2025-05-01 23:27:59 +0600

    Add files via upload

---
+++ Lecture-1/02_DataTypes.js
@@ -0,0 +1,24 @@
+//Data Types-->
+// 1. Primitive(7)--> Number,String,Bool,Undefined,Null,Symbol,BigInt
+// 2. Non-Primitive (Objects-->Arrays, Functions)
+
+
+let fName = "Sabbir";
+console.log(fName);
+console.log(typeof(fName));
+
+let age = 24;
+console.log(typeof(age));
+
+let x;
+console.log(typeof(x));
+
+let y = null;
+console.log(typeof(y));
+
+let z = 123n;
+console.log(typeof(z));
+
+
+
+
