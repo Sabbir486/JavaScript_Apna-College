3c9a3127404ff3f763480b545184ef8166b38e2b
Sabbir Ahmed <124485356+Sabbir486@users.noreply.github.com>
2025-05-02 11:50:37 +0600

    Add files via upload

---
+++ Lecture-2/02_Conditional_Stat.js
@@ -0,0 +1,25 @@
+//Conditional Statement
+
+let age = 17;
+
+if(age > 18){
+    console.log("You can Vote"); 
+}
+
+else{
+    console.log("You can not vote");
+}
+console.log();
+
+
+//Ternary Operators:
+// condition ? true output : false output
+
+let Age = 25;
+
+let result = Age >= 18 ? "adult" : "not adult";
+console.log(result);
+
+
+//switch statement:
+
