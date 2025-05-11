ba7a873a85d7d76f6d665719f63eab7f293b4697
Sabbir Ahmed <australiasabbir355@gmail.com>
2025-05-11 22:07:02 +0600

    Add files via upload

---
+++ Lecture-4/05_Practice.js
@@ -0,0 +1,22 @@
+let companies = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
+
+//Part-1
+companies.shift();
+console.log(companies);
+console.log();
+
+
+//Part-2
+companies.splice(2, 1, "Ola")
+console.log(companies);
+console.log();
+
+
+//Part-3
+companies.push("Amazon");
+console.log(companies);
+console.log();
+
+
+
+
