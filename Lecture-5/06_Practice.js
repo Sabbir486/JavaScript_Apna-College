38a5ec3d16f3875055f17f084609d43ce99fb0f4
Sabbir Ahmed <australiasabbir355@gmail.com>
2025-05-12 23:20:11 +0600

    Add files via upload

---
+++ Lecture-5/06_Practice.js
@@ -0,0 +1,27 @@
+let n = prompt("Enter a number: ");
+
+let arr = [];
+
+for(let i=0; i<n; i++){
+    arr[i] = i+1;
+}
+
+console.log(arr);
+
+//Part-1:
+
+let sum = arr.reduce((prev, curr) => {
+    return prev + curr; 
+})
+
+console.log(sum);
+console.log();
+
+
+//Part-2:
+
+let factorial = arr.reduce((prev, curr) => {
+    return prev * curr; 
+})
+
+console.log(factorial);
