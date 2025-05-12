d678d38d40585802ff40c80e0d8edb12a17f5bc1
Sabbir Ahmed <australiasabbir355@gmail.com>
2025-05-12 23:19:46 +0600

    Add files via upload

---
+++ Lecture-5/02_Arrow_Function.js
@@ -0,0 +1,28 @@
+//Sum Function
+
+const sum = (a, b) => {
+    console.log(a+b);
+    
+}
+
+sum(5, 10);
+
+
+//Multiplication Function
+
+const mul = (c,d) => {
+    console.log(c*d);
+    
+}
+
+mul(10,20);
+console.log();
+
+
+//Print Hello
+
+const printHello = () => {
+    console.log("Hello Print");
+    
+}
+printHello();
