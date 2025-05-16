c3f4ee33cf4f1e4c7fcb5a2da26fc777a0b9f953
Sabbir486 <australiasabbir355@gmail.com>
2025-05-16 20:40:41 +0600

    first commit

---
+++ lec-14/01_practice.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cout<<"My name is John Doe\n";
+    cout<<"I am 20 years old\n";
+    cout<<"I am a student\n";
+    
+    return 0;
+}
