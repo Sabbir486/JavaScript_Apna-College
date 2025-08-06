9036ad76e93a0f54907902fc4c985db944fe0ace
Sabbir Ahmed <australiasabbir355@gmail.com>
2025-08-06 14:48:22 +0600

    Update 03_Practice.js

--- Lecture-2/03_Practice.js
+++ Lecture-2/03_Practice.js
@@ -3,11 +3,12 @@
 let num = prompt("Enter a number: "); //promt --> For taking user input
 
 if(num % 5===0){
-    console.log("Multiple of 5");   
+    console.log(num, " is Multiple of 5");   
 }
 
 else{
-    console.log("Not Multiple of 5");   
+    console.log(num " is Not Multiple of 5");   
 }
+
 
 
