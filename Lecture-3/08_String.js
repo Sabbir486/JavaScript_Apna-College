2562a7e7da46c8498d58b5c646044c14c380faaa
Sabbir486 <australiasabbir355@gmail.com>
2025-08-14 03:52:45 +0600

    String

---
+++ Lecture-3/08_String.js
@@ -0,0 +1,37 @@
+let str = "Sabbir Ahmed";
+console.log(str);
+
+//Properties
+
+let len = str.length;
+console.log(len);
+console.log();
+
+let index = str[2];
+console.log(index);
+console.log();
+
+//Template Literals
+
+let sentence = `I am Sabbir Ahmed`;
+console.log(sentence);
+console.log();
+
+
+// Creating Objects
+
+let obj = {
+    item: "Pen",
+    price: 10,
+};
+
+let output = `The cost of ${obj.item} is ${obj.price} taka`;
+console.log(output);
+console.log();
+
+
+
+
+
+
+
