330c9d69562f89df44de547d208782f16450292e
Sabbir486 <australiasabbir355@gmail.com>
2025-08-14 01:51:17 +0600

    For Loop

---
+++ Lecture-3/01_For_Loop.js
@@ -0,0 +1,18 @@
+// For Loop
+
+for(let i=1; i<=5; i++){
+    console.log("i = ", i);
+}
+
+console.log("Loop has ended");
+console.log()
+
+// Sum of 1 to n
+
+let sum = 0;
+let n = 10;
+for(let i=1; i<=n; i++){
+    sum = sum+i;
+}
+console.log("Sum: ", sum);
+console.log()
