013b0c478bfbef8e43bc7fae80d782d82ee113d5
Sabbir486 <australiasabbir355@gmail.com>
2025-08-14 02:38:26 +0600

    For-Of Loop

---
+++ Lecture-3/04_For_Of_Loop.js
@@ -0,0 +1,13 @@
+// for-of loop
+
+//for use in arrays and strings
+
+let str = "Sabbir Ahmed";
+let size = 0;
+
+for(let i of str){
+    console.log("i : ", i);
+    size++;
+}
+
+console.log("String Size: ", size);
