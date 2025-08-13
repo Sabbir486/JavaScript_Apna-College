2ca7306b64139ebcc9f5ae3a56f9f332c7ed3849
Sabbir486 <australiasabbir355@gmail.com>
2025-08-14 04:03:12 +0600

    String Method

---
+++ Lecture-3/09_String_Method.js
@@ -0,0 +1,24 @@
+// String Methods
+let str = "   Sabbir Ahmed   ";
+
+let met1 = str.toUpperCase();
+console.log(met1);
+console.log();
+
+let met2 = str.trim();
+console.log(met2);
+console.log();
+
+let met3 = str.replace("abb", "bbb");
+console.log(met3);
+console.log();
+
+let met4 = str.charAt(6);
+console.log(met4);
+
+
+
+
+
+
+
