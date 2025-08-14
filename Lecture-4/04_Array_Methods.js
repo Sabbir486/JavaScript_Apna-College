f0b6329e455ed15c9a3d7151f7b3110eaf67dc29
Sabbir Ahmed <australiasabbir355@gmail.com>
2025-08-15 03:48:49 +0600

    Update 04_Array_Methods.js

--- Lecture-4/04_Array_Methods.js
+++ Lecture-4/04_Array_Methods.js
@@ -39,8 +39,16 @@ console.log();
 
 //Method-6: Splice(StartIdx, delCount, newElement) --> Change original array
 
+//Add Element
 let arr = [1,2,3,4,5,6,7];
 console.log(arr.splice(2,0,101,102));
+
+//Delete Element
+console.log(arr.splice(3,1);
+
+//Replace Element
+console.log(arr.splice(3,1,2000);
+
 
 
 
