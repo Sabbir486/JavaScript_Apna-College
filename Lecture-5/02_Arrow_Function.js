e90ab2aec6055e87575f43f60a8c9c6bacabe66c
Sabbir Ahmed <australiasabbir355@gmail.com>
2025-08-15 16:12:25 +0600

    Update 02_Arrow_Function.js

--- Lecture-5/02_Arrow_Function.js
+++ Lecture-5/02_Arrow_Function.js
@@ -1,4 +1,5 @@
-<<<<<<< HEAD
+// Arrow Function
+
 //Sum Function
 
 const sum = (a, b) => {
@@ -26,33 +27,3 @@ const printHello = () => {
     console.log("Hello Print");
     
 }
-=======
-//Sum Function
-
-const sum = (a, b) => {
-    console.log(a+b);
-    
-}
-
-sum(5, 10);
-
-
-//Multiplication Function
-
-const mul = (c,d) => {
-    console.log(c*d);
-    
-}
-
-mul(10,20);
-console.log();
-
-
-//Print Hello
-
-const printHello = () => {
-    console.log("Hello Print");
-    
-}
->>>>>>> 9036ad76e93a0f54907902fc4c985db944fe0ace
-printHello();
