3d0cd4355fe8101523c8ae095d8fa95f85b92bb0
Sabbir Ahmed <australiasabbir355@gmail.com>
2025-08-15 17:08:55 +0600

    Update 06_Practice.js

--- Lecture-5/06_Practice.js
+++ Lecture-5/06_Practice.js
@@ -1,4 +1,5 @@
-<<<<<<< HEAD
+// Problems
+
 let n = prompt("Enter a number: ");
 
 let arr = [];
@@ -26,32 +27,3 @@ let factorial = arr.reduce((prev, curr) => {
 })
 
 console.log(factorial);
-=======
-let n = prompt("Enter a number: ");
-
-let arr = [];
-
-for(let i=0; i<n; i++){
-    arr[i] = i+1;
-}
-
-console.log(arr);
-
-//Part-1:
-
-let sum = arr.reduce((prev, curr) => {
-    return prev + curr; 
-})
-
-console.log(sum);
-console.log();
-
-
-//Part-2:
-
-let factorial = arr.reduce((prev, curr) => {
-    return prev * curr; 
-})
-
-console.log(factorial);
->>>>>>> 9036ad76e93a0f54907902fc4c985db944fe0ace
