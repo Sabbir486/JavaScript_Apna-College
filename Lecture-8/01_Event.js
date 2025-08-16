f8bff9614e7538a24cdee43854e561bf2a1fac89
Sabbir486 <australiasabbir355@gmail.com>
2025-08-17 05:12:22 +0600

    Event Listener

--- Lecture-8/01_Event.js
+++ Lecture-8/01_Event.js
@@ -1,15 +1,50 @@
+// let btn1 = document.querySelector("#btn1");
+
+// btn1.onclick = () => {  //Arrow Function
+//     console.log("Btn1 was clicked");
+//     let a = 25;
+//     a++;
+//     console.log(a);
+    
+// }
+
+// let div = document.querySelector("div");
+
+// div.onmouseover = () => {
+//     console.log("Clicked Done!");
+// }
+
+
+
+// Event Listener -->
+
 let btn1 = document.querySelector("#btn1");
 
-btn1.onclick = () => {  //Arrow Function
-    console.log("Btn1 was clicked");
-    let a = 25;
-    a++;
-    console.log(a);
+btn1.addEventListener("click", (evt) => {
+    console.log("Button was clicked- Handler1");
+    // console.log(evt);
+    // console.log(evt.type);
+       
+})
+
+btn1.addEventListener("click", () => {
+    console.log("Button was clicked- Handler2");
     
-}
+})
 
-let div = document.querySelector("div");
+//Arrow Function
+const handler3 =  () => {
+    console.log("Button was clicked- Handler3");
+};
 
-div.onmouseover = () => {
-    console.log("Clicked Done!");
-}
+btn1.addEventListener("click", handler3);
+
+
+btn1.addEventListener("click", () => {
+    console.log("Button was clicked- Handler4");
+    
+})
+
+// Delete Event Listener
+
+btn1.removeEventListener("click", handler3);
